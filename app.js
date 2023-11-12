83baeddd85e0aa0d206662f6564e2b8d3eaa25b6
nazimboudeffa <boudeffa@gmail.com>
2023-11-12 23:06:30 +0100

    Added first routes

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 import 'dotenv/config'
 import express from 'express';
 import { connectDB } from './db/connection.js';
+import { jobsRoutes } from './routes/jobs.js';
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 9000;
+
+app.use(express.json());
+
+//routes
+app.get('/health', (req, res)=> {
+    res.send('Jobs API is healthy');
+});
+app.use('/', jobsRoutes);
 
 const start = async () => {
     console.log("Starting server...");
