23fd8622a3b4f8eaabb2b7c24eb18386beacc335
Nazim Boudeffa <boudeffa@gmail.com>
2023-12-27 16:39:39 +0100

    Added authentication before create  a thing

--- routes/things.js
+++ routes/things.js
@@ -1,9 +1,10 @@
 import express from 'express';
+import middlewares from '../middlewares/index.js';
 import { createThing, getThings } from '../controllers/things.js';
 
 const router = express.Router();
 
 router.route('/things').get(getThings)
-router.route('/create').post(createThing)
+router.post("/create-thing", middlewares.authenticate, createThing)
 
 export {router as thingsRoutes}
